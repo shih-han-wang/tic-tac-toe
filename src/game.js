45fbddcefa83775460bfac94afedfde63ec978a5
shihhanwang <swang05@mail.bbk.ac.uk>
2018-06-29 12:03:46 +0100

    07: src | Game | initialize p1 & p2

--- src/game.js
+++ src/game.js
@@ -2,11 +2,13 @@
 
 (function(exports){
 
-  function Game(){
+  function Game(p1 = new Player('O', 1), p2 = new Player('X', -1)){
+    this.p1 = p1;
+    this.p2 = p2;
     this._array = [0, 0, 0,
                    0, 0, 0,
                    0, 0, 0];
-  }
+  };
 
   exports.Game = Game;
 })(this);
