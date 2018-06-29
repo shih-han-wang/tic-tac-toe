53b2d81a545e1ee41c367f669d5285753db52e5f
shihhanwang <swang05@mail.bbk.ac.uk>
2018-06-29 11:30:41 +0100

    02: spec | Game function with initial array

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,1 +1,18 @@
 'use strict';
+
+describe('Game', function() {
+
+  var game;
+
+  beforeEach( function() {
+    game = new Game();
+  })
+
+  describe('initial array', function() {
+     it('return array with values 0', function() {
+       expect(game._array).toEqual([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    });
+  });
+
+
+});
