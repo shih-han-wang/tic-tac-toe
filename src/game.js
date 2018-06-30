2b69b5572aa449c06c30511504fec97bd6ec5ff2
shihhanwang <swang05@mail.bbk.ac.uk>
2018-06-30 17:47:39 +0100

    19: src | Game | array name changed and get function added

--- src/game.js
+++ src/game.js
@@ -7,7 +7,7 @@
     this._p2 = p2;
     this._count = 1;
     this._currentPlayer = this._p1;
-    this._array = [0, 0, 0,
+    this._board = [0, 0, 0,
                    0, 0, 0,
                    0, 0, 0];
     this._winSet = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
@@ -18,9 +18,9 @@
   Game.prototype = {
 
     action: function(index){
-      if (this._array[index] === 0) {
+      if (this._board[index] === 0) {
         this._turn();
-        this._array[index] = this._currentPlayer.val;
+        this._board[index] = this._currentPlayer.val;
         this._count++;
       };
     },
@@ -38,15 +38,23 @@
         var set = this._winSet[i];
         var result = 0;
         for(var n = 0; n < 3; n++) {
-          result += this._array[set[n]];
+          result += this._board[set[n]];
           if ( Math.abs(result) === 3 ) {
             return this._currentPlayer.sym + ' WIN!';
           }
         }
       }
+    },
+
+    get: function(){
+      return {
+        player: this._currentPlayer,
+        count: this._count,
+        board: this._board
+      };
     }
 
-  }
+  };
 
   exports.Game = Game;
 })(this);
